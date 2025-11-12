d952d9f50bf553dea5d5e74dce4ead9b25bc717f
hizbullahburiro6-art <hizbullahburiro6@gmail.com>
2025-11-12 17:55:19 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// var fullName = "Hizbullah";
+// alert(fullName);
+// alert(fullName);
+// alert(fullNAme);
+// fullName = "Hizbuillah buriro";
+// alert(fullName);
+// alert(fullName);
+// alert(fullName);
+// var num = 90;
+// alert(num + '5' + 10)
+// var 1st = "text";
+// alert(1st)
+// var alert = "buriro";
+// console.log(alert)
+
+// var fillname = "Hizbullah/buriro";
+// alert(fillname);
+// alert(fillname);
+// alert(fillname);
+
+// var num = 10;
+// alert(num + 5 + '10')
+
+var fullname = "Hizbullah\nBuriro";
+
+alert(fullname);
