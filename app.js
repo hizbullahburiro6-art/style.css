c4297a4799b1327d184b5dfa5887bd7d7641514e
hizbullahburiro6-art <hizbullahburiro6@gmail.com>
2025-11-16 13:25:34 +0500

    fix:

--- app.js
+++ app.js
@@ -43,5 +43,17 @@
 // console.log(x);
 // let x = Math.max (0, 150, 30, 20, -8, -200);
 // console.log(x);
-let x = Math.random()*10;
+// let x = Math.random()*1000;
+// console.log(x);
+// let x = Math.trunc (8.74);
+// console.log(x);
+// let x = Math.PI;
+// console.log(x);
+// 
+
+// let y = 4;
+// x = y * -1;
+// console.log(x);
+let y = 3;
+x = y * -1;
 console.log(x);
